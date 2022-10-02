7aa19bab3d47984b69b18512f8b530ba206ab9ca
DraXyPlayZ-037 <87331129+DraXyPlayZ-037@users.noreply.github.com>
2022-10-02 17:31:27 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+a = "a" //this is just to make github not drop an error (file is empty)
+
+function letgo() { //this is just to make github not drop an error (file is empty)
+    a = "b" //this is just to make github not drop an error (file is empty)
+} //this is just to make github not drop an error (file is empty)
